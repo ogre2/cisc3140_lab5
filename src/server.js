926dd0f9c8139a355304bed0b43f96eae3819ac8
Stephen <hellostephenwm@gmail.com>
2022-05-09 12:35:29 -0400

    Imported config and modified server port

--- src/server.js
+++ src/server.js
@@ -2,10 +2,15 @@
 const express = require('express')
 // Importing http
 const http = require('http')
+// Importing config
+const config = require('./config')
 // Creating express instance
 const app = express()
 // Creating app server
 const server = http.createServer(app)
+
+// Enabling colors
+config.colors.enable()
 
 /**
  * @param NONE
@@ -14,12 +19,16 @@ const server = http.createServer(app)
  * This is a test GET request for the root directory of the API
  */
 app.get('/', (req, res) => {
+    // TODO GET root
     try {
+        // Successful request response
         res.status(200).json({
             message: 'Hello world'
         })
     }
+    // Catch error
     catch(error) {
+        // Unsuccessful request response
         res.status(500).json({
             message: error.message
         })
@@ -32,6 +41,6 @@ app.get('/', (req, res) => {
  * 
  * Launching the API server on port 5000
  */
-server.listen(5000, () => {
-    console.log(`[nodemon] server is running on port:${5000}`)
+server.listen(config.port, () => {
+    console.log(`[nodemon] server is running on port:${config.port}`.green)
 })
