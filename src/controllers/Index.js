0479dbcf2d9891b835e1199a71fb4357e22db84e
Stephen <hellostephenwm@gmail.com>
2022-05-09 13:02:02 -0400

    Controller for index url/view

---
+++ src/controllers/Index.js
@@ -0,0 +1,38 @@
+/**
+ * This controller will server as the application index view contrller.
+ * All methods pertaining to the index view will be housed here.
+ */
+
+// Importing config
+const config = require('../config')
+
+// Enabling colors
+config.colors.enable()
+
+/**
+ * @param {*} req 
+ * @param {*} res 
+ * 
+ * This GET method will return the root application url. If successful
+ * the user will be able to view information located in the root view,
+ * if unsuccessful, the user will be prompted with an error.
+ */
+exports.getIndex = (req, res) => {
+    // TODO GET root url
+    try {
+        // Successful request response
+        res.status(200).json({
+            message: 'Hello world'
+        })
+    }
+    // Catch error
+    catch(error) {
+        // Log the error
+        console.log(error.message.red)
+
+        // Unsuccessful request response
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
