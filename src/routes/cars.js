8e6dcf76209d480383f4f6d4d24895d193656deb
Stephen <hellostephenwm@gmail.com>
2022-05-10 23:12:06 -0400

    More GETs, POST, and PATCH functions

--- src/routes/cars.js
+++ src/routes/cars.js
@@ -16,10 +16,42 @@ config.colors.enable()
 /**
  * @method GET
  * 
- * This method will use the express router to retrieve the cars data from the
- * application database through the cars controller's getAll method.
+ * This method will use the express router to retrieve all of the cars data from the
+ * application database using the controller's getAll method.
  */
 router.get('/cars', carsController.getAll)
+
+/**
+ * @method GET
+ * 
+ * This method will use the express router to retrieve a single car's data by its ID
+ * from the application database using the controller's getID method.
+ */
+router.get('/cars/:id', carsController.getID)
+
+/**
+ * @method GET
+ * 
+ * This method will use the express router to retrieve all car data with the same make
+ * from the application database using the controller's getMake method.
+ */
+router.get('/cars/make/:make', carsController.getMake)
+
+/**
+ * @method POST
+ * 
+ * This method will use the express router to post a new car to the application 
+ * database using the controller's addCar function.
+ */
+router.post('/cars', carsController.addCar)
+
+/**
+ * @method PATCH
+ * 
+ * This method will use the express router to edit an existing car in the application 
+ * database using the controller's updateCar function.
+ */
+router.patch('/cars/:id', carsController.updateCar)
 
 // Exporting router
 module.exports = router
