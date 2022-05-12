f2b48fdfc5a8335a8b1af9a74bf76c99fe9f19b5
Stephen <hellostephenwm@gmail.com>
2022-05-12 02:13:22 -0400

    Initializing asset scripts

---
+++ client/public/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Connected to Main.js file...')
+
+    // Initializing feather icons
+    feather.replace()
+
+    // Initializing AOS
+    AOS.init()
+})
